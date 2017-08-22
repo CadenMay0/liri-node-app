5a52cff74f8fdbeb633c15531dd3a5be44a27816
Caden Mayo <cadenmayo@gmail.com>
2017-08-21 22:00:46 -0400

    Initial structure upload

---
+++ keys.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'aFaPtVIObv5tr9DifOOKnUCnL',
+    consumer_secret: 'b8NjHomLtE3bvAeMy7BKWBTcx7i75v56PRbujeZW7ONEAxZkE3',
+    access_token_key: '898245775372759040-H1hfnxXh1lBYoHsoWIlFpKIkUsoL7hf',
+    access_token_secret: 'ngPjwiEMjPnZl8Ib9pnaDytBbrI10iKcZ2oQHWT9oH1W4',
+}
---
+++ liri.js
@@ -0,0 +1,3 @@
+// JavaScript source code
+var keys = require("./keys.js");
+
